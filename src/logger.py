c3d83472e53e78381e9f17613de2ed3a0cf1295e
ansil <Ansilproabl@gmail.com>
2023-08-19 22:27:24 +0530

    updated looger.py\nadded comments\nsolved some mistakes

--- src/logger.py
+++ src/logger.py
@@ -2,16 +2,21 @@ import logging
 import os
 from datetime import datetime
 
+# Generate a log file name based on the current date and time
+LOG_FILE = f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.log"
 
-LOG_FILE = f"{datetime.now().strftime('%d/%m/%Y,%H:%M:%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Create a path to the 'logs' directory within the current working directory
+logs_dir = os.path.join(os.getcwd(), "logs")
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+# Create the 'logs' directory if it doesn't exist
+os.makedirs(logs_dir, exist_ok=True)
 
+# Create the full path to the log file within the 'logs' directory
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging with the specified settings
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    filename=LOG_FILE_PATH,                  # Use the full path to the log file
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
-    )
-
+    level=logging.INFO,                      # Log messages at the INFO level and higher
+)
