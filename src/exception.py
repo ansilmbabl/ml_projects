f0557f4ec27856ede19257316ffc294c9ad0ac09
ansil <Ansilproabl@gmail.com>
2023-08-19 20:26:26 +0530

    updated exception.py\nadded comments\nremoved unnecessary and confusing variables

--- src/exception.py
+++ src/exception.py
@@ -1,16 +1,25 @@
+# Import the sys module to access system-related functionality
 import sys
 
-def error_message_details(error, error_detail:sys):
-    _, _, exc_tb = error_detail.exc_info()
+# Define a function to create a detailed error message including filename and line number
+def error_message_details(error):
+    # Get information about the current exception using the sys.exc_info() function
+    _, _, exc_tb = sys.exc_info()
+    
+    # Extract the filename and line number from the exception traceback
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = f"Error raised in python script name {file_name} line number {exc_tb.tb_lineno} error message {str(error)}"
     return error_message
-    
-    
+
+# Define a custom exception class that inherits from the built-in Exception class
 class CustomException(Exception):
-    def __init__(self, error_message, error_detail:sys):
-        super.__init__(error_message)
-        self.error_message = error_message_details(error_message, error_detail)
+    def __init__(self, error_message):
+        # Call the constructor of the parent Exception class to initialize the exception
+        super().__init__(error_message)
+        
+        # Generate a detailed error message using the error_message_details function
+        self.error_message = error_message_details(error_message)
        
     def __str__(self) -> str:
-        return self.error_message  
+        # Return the custom error message when the exception is converted to a string
+        return self.error_message
