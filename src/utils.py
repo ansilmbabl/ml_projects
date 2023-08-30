51b032459ef3d3febd08a34b87457c116515fa68
ansil <Ansilproabl@gmail.com>
2023-08-30 19:36:48 +0530

    added comments in save_obj() of utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,13 +3,17 @@ import pickle
 from src.exception import CustomException
 
 
-
 def save_obj(file_path, obj):
     try:
+        # Extract the directory path from the file path
         dir_path = os.path.dirname(file_path)
+        
+        # Ensure that the directory exists or create it
         os.makedirs(dir_path, exist_ok=True)
         
+        # Open the specified file path in binary write mode
         with open(file_path, "wb") as file_obj:
+            # Serialize and write the Python object to the file
             pickle.dump(obj, file_obj)
             
     except Exception as e:
