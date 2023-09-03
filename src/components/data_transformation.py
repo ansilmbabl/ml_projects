5fa7afd0a6609e5befb91f250f36cdcdc00f9cbb
ansil <Ansilproabl@gmail.com>
2023-09-03 20:28:03 +0530

    initiate_data_transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -10,6 +10,7 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
+from utils import save_obj
 
 
 @dataclass
@@ -61,6 +62,43 @@ class DataTransformation:
             
             logging.info("preprocessing completed")
             return preprocessor
+            
+        except Exception as e:
+            raise CustomException(e)
+        
+        
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("read train and test data as dataframes")
+            
+            target_variable = "math_score"
+            input_feature_train_df = train_df.drop(columns = [target_variable], axis = 1)
+            target_feature_train_df = train_df[target_variable]
+            
+            input_feature_test_df = test_df.drop(columns = [target_variable], axis = 1)
+            target_feature_test_df = test_df[target_variable]
+            logging.info("both testing and training data are converted to features and target")
+            
+            preprocessing_object = self.get_data_transformer_object()
+            logging.info("preprocessing object obteained")
+            
+            input_feature_train_arr = preprocessing_object.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_object.transform(input_feature_test_df)
+            logging.info("applied preprocessing transformations on input features")
+            
+            save_obj(
+                file_path= self.data_transformation_config.preprocessor_obj_file_path,
+                obj= preprocessing_object
+            )
+            logging.info("preprocessing object saved")
+            
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+            
+            logging.info("returning train and test data after preprocessing")
+            return (train_arr, test_arr, self.data_transformation_config.preprocessor_obj_file_path)
             
         except Exception as e:
             raise CustomException(e)
