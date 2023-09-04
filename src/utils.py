3389d2ace512cf9a339f96910c814fa490bfad0c
ansil <Ansilproabl@gmail.com>
2023-09-04 19:45:55 +0530

    updated utils\nevaluate model()

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+from sklearn.metrics import r2_score
+
 from src.exception import CustomException
 
 
@@ -16,5 +18,24 @@ def save_obj(file_path, obj):
             # Serialize and write the Python object to the file
             pickle.dump(obj, file_obj)
             
+    except Exception as e:
+        raise CustomException(e)
+    
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models):
+    try:
+        report = {}
+        for item in models:
+            model = models[item]
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            
+            train_r2_score = r2_score(y_test, y_train_pred)
+            test_r2_score = r2_score(y_test, y_test_pred)
+            
+            report[item] = test_r2_score
+        return report
+    
     except Exception as e:
         raise CustomException(e)
