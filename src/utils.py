ae2f0f13d750278b4ecbdc5d9b7f32661b1f476a
ansil <Ansilproabl@gmail.com>
2023-09-05 15:18:37 +0530

    added comments to evaluate model function

--- src/utils.py
+++ src/utils.py
@@ -22,33 +22,48 @@ def save_obj(file_path, obj):
     except Exception as e:
         raise CustomException(e)
     
-    
+
 def evaluate_model(X_train, y_train, X_test, y_test, models, params):
     try:
+        # Initialize an empty dictionary to store evaluation results
         report = {}
+        
+        # Loop through each model specified in the 'models' dictionary
         for item in models:
-            model = models[item]
-            param = params[item]
+            model = models[item]  # Get the model
+            param = params[item]  # Get the hyperparameter grid
             
+            # Create a GridSearchCV instance for hyperparameter tuning
             hyper_params = GridSearchCV(
-                estimator = model,
-                param_grid = param,
-                cv = 3,
-                verbose=2
+                estimator=model,     # Specify the model
+                param_grid=param,   # Specify the hyperparameter grid
+                cv=3,               # Number of cross-validation folds
+                verbose=2           # Verbosity level (2 for detailed output)
             )
+            
+            # Fit the grid search to the training data to find the best hyperparameters
             hyper_params.fit(X_train, y_train)
             
+            # Set the model's hyperparameters to the best values found by GridSearchCV
             model.set_params(**hyper_params.best_params_)
+            
+            # Fit the model with the best hyperparameters on the training data
             model.fit(X_train, y_train)
             
+            # Make predictions on both training and testing data
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             
+            # Compute R-squared scores for both training and testing data
             train_r2_score = r2_score(y_train, y_train_pred)
             test_r2_score = r2_score(y_test, y_test_pred)
             
+            # Store the test R-squared score in the report dictionary
             report[item] = test_r2_score
+        
+        # Return the evaluation report containing test R-squared scores for each model
         return report
     
     except Exception as e:
+        # If an exception occurs, raise a custom exception with the error message
         raise CustomException(e)
